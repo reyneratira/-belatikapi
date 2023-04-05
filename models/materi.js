36aa7e2fb52337b8db504b87a65f71ba358ce727
reyneratira <tneon590@gmail.com>
2023-04-05 21:28:20 +0700

    update-materi

--- models/materi.js
+++ models/materi.js
@@ -3,10 +3,25 @@ module.exports = (sequlize, Sequlize) => {
         judul: {
             type: Sequlize.STRING,
         },
-        gambar: {
-            type: Sequlize.STRING,
+        image: {
+            type: Sequlize.TEXT,
         },
         isi: {
+            type: Sequlize.TEXT,
+        },
+        isi2: {
+            type: Sequlize.TEXT,
+        },
+        isi3: {
+            type: Sequlize.TEXT,
+        },
+        isi4: {
+            type: Sequlize.TEXT,
+        },
+        isi5: {
+            type: Sequlize.TEXT,
+        },
+        isi6: {
             type: Sequlize.TEXT,
         },
     });
